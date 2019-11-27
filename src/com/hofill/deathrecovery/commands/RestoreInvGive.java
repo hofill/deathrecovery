3fd53381f39a3fe703c38fdbed605a237926447b
hofill <nitahteodor@gmail.com>
2019-11-28 01:12:48 +0200

    Changed command name

--- src/com/hofill/deathrecovery/commands/RestoreInvGive.java
+++ src/com/hofill/deathrecovery/commands/RestoreInvGive.java
@@ -4,7 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class RestoreInvEdit implements CommandExecutor{
+public class RestoreInvGive implements CommandExecutor{
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
