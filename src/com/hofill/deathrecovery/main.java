9b4ba7478509eba4cd5b23af8f4f0ba47e210b8f
hofill <nitahteodor@gmail.com>
2019-11-27 23:16:55 +0200

    Skeleton of plugin

---
+++ src/com/hofill/deathrecovery/main.java
@@ -0,0 +1,28 @@
+package com.hofill.deathrecovery;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin{
+
+	public void onEnable() {
+		
+	}
+	
+	public void registerCommands() {
+		
+	}
+	
+	public void registerAutoComplete() {
+		
+	}
+	
+	public void registerEvents() {
+		
+	}
+	
+	public void registerConfig() {
+		getConfig().options().copyDefaults(true);
+		saveDefaultConfig();
+	}
+	
+}
