03304fed7098b85d80bfeb6358d4091130838147
hofill <nitahteodor@gmail.com>
2019-12-04 21:15:53 +0200

    Reformatted

--- src/com/hofill/deathrecovery/commands/RestoreInvGive.java
+++ src/com/hofill/deathrecovery/commands/RestoreInvGive.java
@@ -16,112 +16,103 @@ import net.md_5.bungee.api.ChatColor;
 
 public class RestoreInvGive implements CommandExecutor {
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-		if (label.equalsIgnoreCase("restoreinvgive") || label.equalsIgnoreCase("rig")) {
-			if (args.length >= 2 && args.length <= 3) {
-				if (sender instanceof Player) {
-					if (parseInt(args[1]) != -1) {
-						Player player = (Player) sender;
-						if (player.hasPermission("deathrecovery.restoregive")) {
-							OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[0]);
-							OfflinePlayer playerToAdd = null;
-							String playerUUID = offlinePlayer.getUniqueId().toString();
-							ConfigurationSection sectionDeaths = ConfigManager.getConfig()
-									.getConfigurationSection("players." + playerUUID + "." + args[1] + ".items");
-							if (args.length == 3) {
-								playerToAdd = Bukkit.getOfflinePlayer(args[2]);
-							} else {
-								playerToAdd = Bukkit.getOfflinePlayer(args[0]);
-							}
-							// Check if there are any items in the death inventory.
-							if (sectionDeaths != null) {
-								// Check if player is online.
-								if (playerToAdd.isOnline()) {
-									// Check if his inventory is empty.
-									if (getFull(playerToAdd.getPlayer().getInventory())) {
-										// Add items to the player's inventory
-										fillInv(sectionDeaths, playerUUID, playerToAdd.getPlayer(), args[1]);
-//										for (String items : sectionDeaths.getKeys(false)) {
-//											int itemIndex = parseInt(items);
-//											ItemStack itemToAdd = ConfigManager.getConfig().getItemStack(
-//													"players." + playerUUID + "." + args[1] + ".items." + items);
-//											playerToAdd.getPlayer().getInventory().setItem(itemIndex, itemToAdd);
-//										}
-										player.sendMessage(ChatColor.GREEN + "Restored " + playerToAdd.getName()
-												+ "'s inventory successfully!");
-									} else {
-										player.sendMessage(ChatColor.RED + args[0] + "'s inventory is not empty!");
-									}
-									// If player is offline, wait for him to come online
-								} else {
-									String offlinePlayerUUID = playerToAdd.getUniqueId().toString();
-									ConfigurationSection sectionAddOffline = ConfigManager.getConfig()
-											.getConfigurationSection("offline_players." + offlinePlayerUUID);
-									if (sectionAddOffline == null) {
-										ConfigManager.getConfig()
-												.set("offline_players." + offlinePlayerUUID + ".inv_name", playerUUID);
-										ConfigManager.getConfig()
-												.set("offline_players." + offlinePlayerUUID + ".death_id", args[1]);
-										ConfigManager.saveConfig();
-										ConfigManager.reloadConfig();
-										player.sendMessage(ChatColor.GREEN + "Added " + playerToAdd.getName()
-												+ " to the waiting list successfully!");
-									} else {
-										player.sendMessage(ChatColor.RED
-												+ "This player was already going to recieve his inventory!");
-									}
-								}
-							} else {
-								player.sendMessage(ChatColor.RED + "The player or the death ID does not exist!");
-								return true;
-							}
+        if (label.equalsIgnoreCase("restoreinvgive") || label.equalsIgnoreCase("rig")) {
+            if (!(args.length >= 2 && args.length <= 3)) {
+                return false;
+            }
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Only players can use this command!");
+                return true;
+            }
+            if (parseInt(args[1]) == -1) {
+                sender.sendMessage(ChatColor.RED + "The death ID is a number bigger than 0!");
+                return false;
+            }
+            Player player = (Player) sender;
+            if (player.hasPermission("deathrecovery.restoregive")) {
+                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[0]);
+                OfflinePlayer playerToAdd = null;
+                String playerUUID = offlinePlayer.getUniqueId().toString();
+                ConfigurationSection sectionDeaths = ConfigManager.getConfig()
+                        .getConfigurationSection("players." + playerUUID + "." + args[1] + ".items");
+                if (args.length == 3) {
+                    playerToAdd = Bukkit.getOfflinePlayer(args[2]);
+                } else {
+                    playerToAdd = Bukkit.getOfflinePlayer(args[0]);
+                }
+                // Check if there are any items in the death inventory.
+                if (sectionDeaths != null) {
+                    // Check if player is online.
+                    if (playerToAdd.isOnline()) {
+                        // Check if his inventory is empty.
+                        if (getFull(playerToAdd.getPlayer().getInventory())) {
+                            // Add items to the player's inventory
+                            fillInv(sectionDeaths, playerUUID, playerToAdd.getPlayer(), args[1]);
+                            player.sendMessage(ChatColor.GREEN + "Restored " + playerToAdd.getName()
+                                    + "'s inventory successfully!");
+                        } else {
+                            player.sendMessage(ChatColor.RED + args[0] + "'s inventory is not empty!");
+                        }
+                        // If player is offline, wait for him to come online
+                    } else {
+                        String offlinePlayerUUID = playerToAdd.getUniqueId().toString();
+                        ConfigurationSection sectionAddOffline = ConfigManager.getConfig()
+                                .getConfigurationSection("offline_players." + offlinePlayerUUID);
+                        if (sectionAddOffline == null) {
+                            ConfigManager.getConfig().set("offline_players." + offlinePlayerUUID + ".inv_name",
+                                    playerUUID);
+                            ConfigManager.getConfig().set("offline_players." + offlinePlayerUUID + ".death_id",
+                                    args[1]);
+                            ConfigManager.saveConfig();
+                            ConfigManager.reloadConfig();
+                            player.sendMessage(ChatColor.GREEN + "Added " + playerToAdd.getName()
+                                    + " to the waiting list successfully!");
+                        } else {
+                            player.sendMessage(
+                                    ChatColor.RED + "This player was already going to recieve his inventory!");
+                        }
+                    }
+                } else {
+                    player.sendMessage(ChatColor.RED + "The player or the death ID does not exist!");
+                    return true;
+                }
 
-						} else {
-							player.sendMessage(ChatColor.RED + "You don't have permission!");
-							return true;
-						}
-					} else {
-						sender.sendMessage(ChatColor.RED + "The death ID is a number bigger than 0!");
-						return false;
-					}
-				} else {
-					sender.sendMessage("Only players can use this command!");
-					return true;
-				}
-			} else {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	public static void fillInv(ConfigurationSection cs, String playerUUID, Player playerToAdd, String death_id) {
-		for (String items : cs.getKeys(false)) {
-			int itemIndex = parseInt(items);
-			ItemStack itemToAdd = ConfigManager.getConfig().getItemStack(
-					"players." + playerUUID + "." + death_id + ".items." + items);
-			playerToAdd.getInventory().setItem(itemIndex, itemToAdd);
-		}
-	}
+            } else {
+                player.sendMessage(ChatColor.RED + "You don't have permission!");
+                return true;
+            }
+        }
+        return true;
+    }
 
-	private boolean getFull(Inventory inv) {
-		for (ItemStack item : inv.getContents()) {
-			if (item != null)
-				return false;
-		}
-		return true;
-	}
+    public static void fillInv(ConfigurationSection cs, String playerUUID, Player playerToAdd, String death_id) {
+        for (String items : cs.getKeys(false)) {
+            int itemIndex = parseInt(items);
+            ItemStack itemToAdd = ConfigManager.getConfig()
+                    .getItemStack("players." + playerUUID + "." + death_id + ".items." + items);
+            playerToAdd.getInventory().setItem(itemIndex, itemToAdd);
+        }
+    }
 
-	private static int parseInt(String string) {
-		int x = -1;
-		try {
-			x = Integer.parseInt(string);
-		} catch (Exception ex) {
-		}
-		return x;
-	}
+    private boolean getFull(Inventory inv) {
+        for (ItemStack item : inv.getContents()) {
+            if (item != null)
+                return false;
+        }
+        return true;
+    }
+
+    private static int parseInt(String string) {
+        int x = -1;
+        try {
+            x = Integer.parseInt(string);
+        } catch (Exception ex) {
+        }
+        return x;
+    }
 
 }
